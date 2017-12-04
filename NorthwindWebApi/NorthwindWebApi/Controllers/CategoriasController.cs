0e54561d5ed0ae9e5f4eba848428c409509c4a9a
Seregor <Seregor@SEREGOR-PC>
2017-12-04 18:40:30 -0500

    Modificacion en controller

---
+++ NorthwindWebApi/NorthwindWebApi/Controllers/CategoriasController.cs
@@ -0,0 +1,25 @@
+﻿using NorthwindWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NorthwindWebApi.Controllers
+{
+    public class CategoriasController : ApiController
+    {
+        [HttpGet]
+        [Route("api/categorias/consultar")]
+        public List<Categories> consultarCategorias()
+        {
+            using (var cnn = new NorthwindEntities())
+            {
+                return cnn.Categories.ToList();
+            }
+        }
+           
+
+    }
+}
